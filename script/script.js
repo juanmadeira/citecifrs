5867c5d4932417142f25d0eab8f21dc41383c5eb
juan <juanpmad@outlook.com>
2023-06-16 09:49:41 -0300

    script

--- script/script.js
+++ script/script.js
@@ -11,7 +11,7 @@ let lmenu = document.querySelector(".menu");
 
 bmenu.addEventListener('click', e => {showMenu()});
 
-function showMenu(){
+function showMenu() {
     if (bmenu.className === "nav-icon") {
         bmenu.className += " active";
         lmenu.className += " active";
