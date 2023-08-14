66d7b6e08164a6b608972deec90630e16dc1451a
juan <juanpmad@outlook.com>
2023-08-14 11:53:57 -0300

    view

--- view/header.php
+++ view/header.php
@@ -1,23 +1,21 @@
-<header>
-    <div class="desktop-menu">
-        <a href="./index.html" class="header-link hover-u">Início</a>
-        <a href="./projetos.html" class="header-link hover-u">Projetos</a>
-        <a href="./" class="hover-u"><img src="./img/logo.png" alt="CITec - IFRS Logo"></a>
-        <a href="./agenda.html" class="header-link hover-u">Agenda</a>
-        <a href="./sobre.html" class="header-link hover-u">Sobre</a>
+<div class="desktop-menu">
+    <a href="./index.html" class="header-link hover-u">Início</a>
+    <a href="./projetos.html" class="header-link hover-u">Projetos</a>
+    <a href="./" class="hover-u"><img src="./img/logo.png" alt="CITec - IFRS Logo"></a>
+    <a href="./agenda.html" class="header-link hover-u">Agenda</a>
+    <a href="./sobre.html" class="header-link hover-u">Sobre</a>
+</div>
+<div class="mobile-menu">
+    <a href="./"><img src="./img/logo.png" alt="CITec - IFRS Logo"></a>
+    <div class="nav-icon" onclick="showMenu()">
+        <span></span>
+        <span></span>
+        <span></span>
     </div>
-    <div class="mobile-menu">
-        <a href="./"><img src="./img/logo.png" alt="CITec - IFRS Logo"></a>
-        <div class="nav-icon" onclick="showMenu()">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <ul class="menu">
-            <li class="list"><a href="./index.html" class="header-link hover-u">Início</a></li>
-            <li class="list"><a href="./projetos.html" class="header-link hover-u">Projetos</a></li>
-            <li class="list"><a href="./agenda" class="header-link hover-u">Agenda</a></li>
-            <li class="list"><a href="./sobre.html" class="header-link hover-u">Sobre</a></li>
-        </ul>
-    </div>
-</header>
+    <ul class="menu">
+        <li class="list"><a href="./index.html" class="header-link hover-u">Início</a></li>
+        <li class="list"><a href="./projetos.html" class="header-link hover-u">Projetos</a></li>
+        <li class="list"><a href="./agenda" class="header-link hover-u">Agenda</a></li>
+        <li class="list"><a href="./sobre.html" class="header-link hover-u">Sobre</a></li>
+    </ul>
+</div>
