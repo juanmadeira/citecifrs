366b99fbed96660728463b3006b568a1b0f9276f
juan <juanpmad@outlook.com>
2023-09-11 10:53:58 -0300

    view

---
+++ view/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand hover-u" href="#"><img src="./img/logo.png" alt="Logo CITec"></a>
+        <button class="m-3 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item active">
+                <a class="nav-link hover-u" href="./index.html">Início</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link hover-u" href="./projetos.html">Projetos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link hover-u" href="./agenda.html">Agenda</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link hover-u" href="./sobre.html">Sobre</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
