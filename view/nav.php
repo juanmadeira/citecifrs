590e55c81e43c8ea90404cfbd61fb65f66226048
juan <juanpmad@outlook.com>
2023-09-15 11:14:29 -0300

    view

--- view/nav.php
+++ view/nav.php
@@ -9,8 +9,14 @@
                 <li class="nav-item active">
                     <a class="nav-link hover-u" href="./index.html">INÍCIO</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link hover-u" href="./projetos.html">PROJETOS</a>
+                <li class="nav-item dropdown">
+                    <button class="btn btn-default btn-lg px-0 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="hover-u">PROJETOS</span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="./projetos.html"><span class="hover-u">GALERIA</span></a></li>
+                        <li><a class="dropdown-item" href="./cadastro.html"><span class="hover-u">CADASTRO</span></a></li>
+                    </ul>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link hover-u" href="./agenda.html">AGENDA</a>
