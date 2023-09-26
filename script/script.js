dcb1b57433558e69238239cb003569fe01185f64
Juan <juanpmad@outlook.com>
2023-09-26 16:05:05 -0300

    script

--- script/script.js
+++ script/script.js
@@ -4,54 +4,27 @@ const qs = (val) => {
 }
 
 /* owl carousel */
-/* $(".inicio-carousel").on("initialized.owl.carousel", () => {
-    setTimeout(() => {
-        $(".owl-item.active .owl-slide-animated").addClass("is-transitioned");
-        $("section").show();
-    }, 200);
+$('.inicio-carousel').owlCarousel({
+    animateOut: 'slideOutUp',
+    autoplayTimeout:2000,
+    autoplay:true,
+    dots:false,
+    loop:true,
+    margin:0,
+    mouseDrag:false,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
 });
-
-const $inicioCarousel = $(".inicio-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    nav: true,
-    navText: [
-        '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
-        '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>' // icons from https://iconmonstr.com
-    ]
-});
-
-$inicioCarousel.on("changed.owl.carousel", e => {
-    $(".owl-slide-animated").removeClass("is-transitioned");
-    const $currentOwlItem = $(".owl-item").eq(e.item.index);
-    $currentOwlItem.find(".owl-slide-animated").addClass("is-transitioned");
-    const $target = $currentOwlItem.find(".owl-slide-text");
-    doDotsCalculations($target);
-});
-
-$inicioCarousel.on("resize.owl.carousel", () => {
-    setTimeout(() => {
-    setOwlDotsPosition();
-    }, 50);
-});
-
-setOwlDotsPosition();
-
-function setOwlDotsPosition() {
-    const $target = $(".owl-item.active .owl-slide-text");
-    doDotsCalculations($target);
-}
-
-function doDotsCalculations(el) {
-    const height = el.height();
-    const {top, left} = el.position();
-    const res = height + top + 20;
-
-    $(".inicio-carousel .owl-dots").css({
-        top: `${res}px`,
-        left: `${left}px`
-    });
-} */
 
 $('.lab-carousel').owlCarousel({
     autoplay:true,
