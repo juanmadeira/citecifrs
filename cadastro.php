c6a325ad18b58025a137586f0ffcc0b8967325d9
Juan <juanpmad@outlook.com>
2023-10-20 11:53:19 -0300

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -29,7 +29,7 @@
                     <?php
                         $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
                         if (!empty($dados['submit'])) {
-                            include "db_conn.php";
+                            include "./config/db_conn.php";
                             var_dump($dados);
 
                             // validar o campo individual
