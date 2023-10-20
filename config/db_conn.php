95e3cfdb4aab8fa721cebf1ae24f16b6974be492
Juan <juanpmad@outlook.com>
2023-10-19 21:20:26 -0300

    config

---
+++ config/db_conn.php
@@ -0,0 +1,13 @@
+<?php  
+    $sname = "localhost";
+    $uname = "root";
+    $password = "";
+    $db_name = "citecifrs";
+    
+    $conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+    if (!$conn) {
+        echo "conexão falhou!";
+        exit();
+    }
+?>
---
+++ config/projeto_upload.php
@@ -0,0 +1,49 @@
+<?php
+    if (isset($_POST['submit']) && isset($_FILES['imagem'])) {
+        include "db_conn.php";
+
+        echo "<pre>";
+        print_r($_FILES['imagem']);
+        echo "</pre>";
+
+        $img_name = $_FILES['imagem']['name'];
+        $img_size = $_FILES['imagem']['size'];
+        $tmp_name = $_FILES['imagem']['tmp_name'];
+        $error = $_FILES['imagem']['error'];
+
+        if ($error === 0) {
+            if ($img_size > 1) {
+                $em = "Desculpe! Sua imagem é muito grande.";
+                header ("Location: ../cadastro.php?error=$em");
+            } else {
+
+                $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+                $img_ex_lc = strtolower($img_ex);
+
+                $allowed_exs = array("jpg", "jpeg", "png"); 
+
+                if (in_array($img_ex_lc, $allowed_exs)) {
+                    date_default_timezone_set("Brazil/East");
+                    $ext = strtolower(substr($_FILES['imagem']['name'],-4));
+
+                    $new_img_name = date("Y-m-d-H.i.s") . $ext;
+                    $img_upload_path = '../uploads/'.$new_img_name;
+                    move_uploaded_file($tmp_name, $img_upload_path);
+
+                    $sql = "INSERT INTO projetos(image_url) 
+                            VALUES('$new_img_name')";
+                    mysqli_query($conn, $sql);
+                    header ("Location: ../cadastro-sucess.php");
+                } else {
+                    $em = "Formato de imagem inválido.";
+                    header ("Location: ../cadastro.php?error=$em");
+                }
+            }
+        } else {
+            $em = "Um erro inesperado ocorreu!";
+            header ("Location: ../cadastro.php?error=$em");
+        }
+    } else {
+        header ("Location: ../cadastro.php");
+    }
+?>
